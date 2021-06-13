e182d11efabcc471c8f9f65b2c7ed609ce303e9d
Guru Kishore <guru.kishore@wylo.in>
2021-06-13 15:18:37 +0530

    Completed app

---
+++ app/components/rentals/filter.js
@@ -0,0 +1,11 @@
+import Component from '@glimmer/component';
+
+export default class RentalsFilterComponent extends Component {
+  get results() {
+    let { rentals, query } = this.args;
+    if (query) {
+      rentals = rentals.filter((rental) => rental.title.includes(query));
+    }
+    return rentals;
+  }
+}
